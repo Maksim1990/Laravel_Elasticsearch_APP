9afc614a6fc8464fe6802631e1ec74611ab50793
Your Name <you@example.com>
2018-06-16 00:02:03 +0300

    Added simple Laravel application with implementation of Elasticsearch functionality

---
+++ app/Providers/AppServiceProvider.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Providers;
+
+use App\Elastic\Elastic;
+use App\Post;
+use Elasticsearch\ClientBuilder;
+use Illuminate\Support\ServiceProvider;
+
+class AppServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $elastic = $this->app->make(Elastic::class);
+
+        Post::saved(function ($post) use ($elastic) {
+            $elastic->index([
+                'index' => 'blog',
+                'type' => 'post',
+                'id' => $post->id,
+                'body' => $post->toArray()
+            ]);
+        });
+
+        Post::deleted(function ($post) use ($elastic) {
+            $elastic->delete([
+                'index' => 'blog',
+                'type' => 'post',
+                'id' => $post->id,
+            ]);
+        });
+    }
+
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->app->bind(Elastic::class, function ($app) {
+            return new Elastic(
+                ClientBuilder::create()
+                    ->setLogger(ClientBuilder::defaultLogger(storage_path('logs/elastic.log')))
+                    ->build()
+            );
+        });
+    }
+}
